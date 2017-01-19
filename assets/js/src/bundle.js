596a22d81d4bd64738e0793b02866ec33c64b0a1
Luka Mis <luka.mis@gmail.com>
2017-01-19 10:29:46 +0100

    Classes for game deleted. They were moved to parent assets folder as different games share same code just with different color themes.

--- assets/js/src/bundle.js
+++ assets/js/src/bundle.js
@@ -1,20 +1,3 @@
 
 
-// @prepros-append entry.js
-// @prepros-append ThemeControler.js
-// @prepros-append DataHandler.js
-
-// @prepros-append TitleView.js
-// @prepros-append FeedbackView.js
-// @prepros-append InfoBar.js
-// @prepros-append SpawnControl.js
-// @prepros-append GameControl.js
-
-// @prepros-append SoundControl.js
-
-// @prepros-append Particles.js
-
- 
- // @prepros-append Bootstrap.js
-
  // @prepros-append init.js
